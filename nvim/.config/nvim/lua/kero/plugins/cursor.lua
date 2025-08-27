a814a30f84760f476e5237d1d0c30ae96d672a35
Michael Ker0 <michael.kero@ambr.net>
2025-08-27 15:10:07 -0400

    cursor add and close openvpn script

--- nvim/.config/nvim/lua/kero/plugins/cursor.lua
+++ nvim/.config/nvim/lua/kero/plugins/cursor.lua
@@ -1,8 +1,17 @@
 return {
-  "xTacobaco/cursor-agent.nvim",
+  dir = "~/automata/repos/xTacobaco/cursor-agent.nvim",
+  -- "xTacobaco/cursor-agent.nvim",
   config = function()
     vim.keymap.set("n", "<leader>ca", ":CursorAgent<CR>", { desc = "Cursor Agent: Toggle terminal" })
     vim.keymap.set("v", "<leader>ca", ":CursorAgentSelection<CR>", { desc = "Cursor Agent: Send selection" })
     vim.keymap.set("n", "<leader>cA", ":CursorAgentBuffer<CR>", { desc = "Cursor Agent: Send buffer" })
+    require("cursor-agent").setup({
+      window = {
+        layout = "split",
+        split = {
+            side = 'bottom'
+        }
+      }
+    })
   end,
 }
