5146619ec1d0a74e14940f86a7beeb768d340d0e
Michael Kero <michael.kero@motif.io>
2026-06-25 22:34:30 -0400

    stuff

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -1,12 +1,12 @@
 return {
-    "sho-87/kanagawa-paper.nvim",
-    -- 'AlexvZyl/nordic.nvim',
+    -- "sho-87/kanagawa-paper.nvim",
+    'AlexvZyl/nordic.nvim',
     -- 'rose-pine/neovim',
     -- "catppuccin/nvim", 
     priority = 1000,
     config = function ()
-        vim.cmd.colorscheme("kanagawa-paper")
-        -- vim.cmd.colorscheme("nordic")
+        -- vim.cmd.colorscheme("kanagawa-paper")
+        vim.cmd.colorscheme("nordic")
         -- vim.cmd.colorscheme("rose-pine")
         -- vim.cmd.colorscheme("catppuccin-macchiato")
 
